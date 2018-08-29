10d05563ffffff0903e915b3b8702965a762d0dc
unknown <pablo.souza18@hotmail.com>
2018-08-28 23:34:00 -0300

    First Commit

---
+++ base.js
@@ -0,0 +1,61 @@
+var verbs = [
+    {
+        'infinitive': 'write',
+        'past': 'wrote',
+        'participle': 'write',
+    },
+
+    {
+        'infinitive': 'forgive',
+        'past': 'forgave',
+        'participle': 'forgiven',
+    },
+
+    {
+        'infinitive': 'shrink',
+        'past': 'shrank',
+        'participle': 'shrunk',
+    },
+
+    {
+        'infinitive': 'see',
+        'past': 'saw',
+        'participle': 'seen',
+    },
+
+    {
+        'infinitive': 'bind',
+        'past': 'bound',
+        'participle': 'bound',
+    },
+
+    {
+        'infinitive': 'breed',
+        'past': 'bred',
+        'participle': 'bred',
+    },
+
+    {
+        'infinitive': 'choose',
+        'past': 'chose',
+        'participle': 'chosen',
+    },
+
+    {
+        'infinitive': 'drink',
+        'past': 'drank',
+        'participle': 'drunk',
+    },
+
+    {
+        'infinitive': 'fling',
+        'past': 'flung',
+        'participle': 'flung',
+    },
+
+    {
+        'infinitive': 'have',
+        'past': 'had',
+        'participle': 'had',
+    },
+]
